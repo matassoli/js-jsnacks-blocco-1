323e6c20d74255adeb5fb63aba5e9f7d89e49e39
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-02 11:39:08 +0100

    snack2 fix

--- snack2/myscript.js
+++ snack2/myscript.js
@@ -1,14 +1,10 @@
-// L’utente inserisce due parole in successione, con due
-// prompt. Il software stampa prima la parola più corta, poi la
-// parola più lunga.
-
-
 var word1 = prompt("Inserisci la prima parola:");
 var word2 = prompt("Inserisci la seconda parola:");
 
 if (word1.length < word2.length) {
-  document.getElementById("word").innerHTML = "La parola più corta è " + word1 + " la più lunga è " + word2;
-}
-if (word1.length > word2.length) {
-  document.getElementById("word").innerHTML = "La parola più corta è " + word2 + " la più lunga è " + word1;
+  document.getElementById("word").innerHTML = "La parola più corta è " + word1 + ", la più lunga è " + word2 + ".";
+} else if (word1.length > word2.length) {
+  document.getElementById("word").innerHTML = "La parola più corta è " + word2 + ", la più lunga è " + word1 + ".";
+} else if (word1.length == word2.length) {
+  document.getElementById("word").innerHTML = "Le parole hanno la stessa lunghezza.";
 }
