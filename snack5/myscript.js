bcb4ea734111d7a5b454d4b9190199b28c1f3358
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-02 13:00:38 +0100

    snack5 fix

--- snack5/myscript.js
+++ snack5/myscript.js
@@ -1,9 +1,6 @@
-// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
-// un numero, se è dispari inseriscilo nell’array.
-
 var numDispari = [];
 
-for (var i = 0; i <= 5; i++) {
+for (var i = 1; i <= 6; i++) {
   var numero = parseInt(prompt("Inserisci un numero"));
   if (numero % 2 == 1) {
     numDispari.push(numero);
