44358f9a58d5511ac67d078527dea510d5251bc6
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-03 12:11:34 +0100

    snack6 do-while

--- snack6/myscript.js
+++ snack6/myscript.js
@@ -1,4 +1,6 @@
-var numero = parseInt(prompt("Scrivi un numero"));
+do {
+  var numero = parseInt(prompt("Scrivi un numero"));
+} while (isNaN(numero));
 
 for (var i = 1; i <= numero; i++) {
   console.log(Math.pow(i, 3));
